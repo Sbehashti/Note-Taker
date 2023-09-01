08d56bce34607de7c4f336a3fc4564dd408f4c13
Sier Behashti <sbehashti@gmail.com>
2023-08-31 18:11:57 -0700

    removed Develop Folder

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ const writeFileAsync = util.promisify(fs.writeFile);
 //Server 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 //Middleware
-app.use(express.static('./Develop/public/assets/js/server.js'));
+app.use(express.static('public'));
 
 //API route; GET 
 app.get('/api/notes', function(req, res){
-    readFileAsync("./Develop/db/db.json", 'utf8').then(function(data){
+    readFileAsync("./db/db.json", 'utf8').then(function(data){
         notes = [].concat(JSON.parse(data))
         res.json(notes);
     })
@@ -28,13 +28,13 @@ app.get('/api/notes', function(req, res){
 //API route; POST
 app.post('/api/notes', function(req,res){
     const note = req.body;
-    readFileAsync('./Develop/db/db.json', 'utf8').then(function(data){
+    readFileAsync('./db/db.json', 'utf8').then(function(data){
         const notes = [].concat(JSON.parse(data));
         note.id = notes.length + 1
         notes.push(note);
         return notes
     }).then(function(notes){
-        writeFileAsync("./Develop/db/db.json", JSON.stringify(notes))
+        writeFileAsync("./db/db.json", JSON.stringify(notes))
         res.json(note);
 })
 
@@ -43,7 +43,7 @@ app.post('/api/notes', function(req,res){
 //API route; DELETE
 app.delete('/api/notes/:id', function(req, res){
     const idToDelete = parseInt(req.params.id);
-    readFileAsync('./Develop/db/db.json', 'utf8').then(function(data){
+    readFileAsync('./db/db.json', 'utf8').then(function(data){
         const notes = [].concat(JSOn.parse(data));
         const newNotesData = []
         for (let i = 0; i<notes.length; i++){
@@ -53,20 +53,20 @@ app.delete('/api/notes/:id', function(req, res){
         }
         return newNotesData
     }).then(function(notes){
-        writeFileAsync('./Develop/db/db.json', JSON.stringify(notes))
+        writeFileAsync('./db/db.json', JSON.stringify(notes))
         res.send('Saved Success');
     })
 });
 
 //HTML route
 app.get("/notes", function(req,res){
-    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 app.get('*', function(req,res){
-    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 //Listen
